a3b74e8450133c694ee947efd94b0c12a86a00bf
Ankit Kumar <ankitkr5858@gmail.com>
2020-04-16 16:55:33 +0300

    update profile PATCH endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,4 +10,15 @@ router.get('/profile', asyncHandler(async (req, res, next) => {
     return res.send(user);
 }));
 
+router.patch('/profile', asyncHandler(async (req, res, next) => {
+    const user = await UserService.findById(req.user.userId);
+    const payload = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        invitedByReferralCode: req.body.invitedByReferralCode
+    };
+    await UserService.update(user, payload);
+    return res.send(user);
+}));
+
 module.exports = router;

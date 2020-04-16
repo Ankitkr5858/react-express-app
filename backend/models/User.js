b4303d9390e1440c8917b483fee0b4f652d11a0a
Ankit Kumar <ankitkr5858@gmail.com>
2020-04-16 08:50:03 +0300

    calculate and serialize User#isProfileComplete field

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,32 +2,41 @@ const mongoose = require('mongoose');
 const {generateReferralCode} = require('../helpers/referral_code_helpers');
 
 const userSchema = mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        lowercase: true
-    },
-    firstName: {
-        type: String
-    },
-    lastName: {
-        type: String
-    },
-    referralCode: {
-        type: String,
-        required: true
-    },
-    invitedByReferralCode: {
-        type: String
-    },
-    otp: {
-        type: String
+        email: {
+            type: String,
+            required: true,
+            lowercase: true
+        },
+        firstName: {
+            type: String
+        },
+        lastName: {
+            type: String
+        },
+        referralCode: {
+            type: String,
+            required: true
+        },
+        invitedByReferralCode: {
+            type: String
+        },
+        otp: {
+            type: String
+        }
+    }, {
+        toJSON: {
+            virtuals: true
+        }
     }
-});
+);
 
-userSchema.pre('validate', function(next) {
+userSchema.pre('validate', function (next) {
     if (this.isNew) this.referralCode = generateReferralCode();
     next();
+});
+
+userSchema.virtual('isProfileComplete').get(function () {
+    return (this.firstName !== undefined && this.lastName !== undefined);
 });
 
 module.exports = mongoose.model('user', userSchema);

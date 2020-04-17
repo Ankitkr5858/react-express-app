4a6019f09be014703c4be9cdd41a8af6610cef55
Ankit Kumar <ankitkr5858@gmail.com>
2020-04-17 06:18:25 +0300

    fix authentication reducer

--- client/src/reducers/authentication.reducer.js
+++ client/src/reducers/authentication.reducer.js
@@ -1,20 +1,23 @@
 import {AuthenticationConstants} from '../constants/authentication.constants';
+import {UserConstants} from '../constants/user.constants';
 
 const user = JSON.parse(localStorage.getItem('user'));
 const initialState = user ? {user} : {};
 
 export function authenticationReducer(state = initialState, action) {
-    const {type, user, accessToken} = action;
+    const {type, user, accessToken, invitedByReferralCode} = action;
 
     switch (type) {
         case AuthenticationConstants.LOGIN_SUCCESS:
             return {user};
         case AuthenticationConstants.OTP_SUCCESS:
-            return {
-                user: {...state.user, accessToken}
-            };
+            return {user: {...state.user, accessToken}};
         case AuthenticationConstants.LOGOUT:
             return {};
+        case UserConstants.REFERRAL_CODE_PERSISTED:
+            return {...state, invitedByReferralCode};
+        case UserConstants.PROFILE_UPDATE_SUCCESS:
+            return {user: {...state.user, ...user}};
         default:
             return state
     }

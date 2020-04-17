a33ed337c691def364161d630ce9e483b6777f85
Ankit Kumar <ankitkr5858@gmail.com>
2020-04-17 17:16:41 +0300

    built client

---
+++ backend/public/precache-manifest.93503068dce3906d2ca6649955e7a08b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54322410da7a14c99f08a5f1f24e6ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eecb05d5751b1b5e2a3",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a0572f5bca1747342be3",
+    "url": "/static/css/main.96a6297b.chunk.css"
+  },
+  {
+    "revision": "0eecb05d5751b1b5e2a3",
+    "url": "/static/js/2.13d09bb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.13d09bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0572f5bca1747342be3",
+    "url": "/static/js/main.406a9843.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
